aa4dbd8b46b18ebca66f0638c36c6a91c4d3456d
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-12-25 00:42:20 +0300

    Dsa 2 Collections Reviews

--- dsa_01_AlgoritmAnalysis/src/main/java/org/example/assignments/Assignment1_TwoSumOptimal.java
+++ dsa_01_AlgoritmAnalysis/src/main/java/org/example/assignments/Assignment1_TwoSumOptimal.java
@@ -6,18 +6,22 @@ import java.util.Map;
 
 public class Assignment1_TwoSumOptimal {
     public static void main(String[] args) {
-        int[] array=new int[]{2,7,9,11};
+        int[] array = new int[]{2, 7, 9, 11};
         System.out.println(Arrays.toString(twoSumOptimalSolution(array, 9)));
     }
-    public static int[] twoSumOptimalSolution(int[] array, int targetValue){
+
+    public static int[] twoSumOptimalSolution(int[] array, int targetValue) {
         // Complexity of the solution is O(n) but I have space complexity of O(n)
         // create hashmap
-        Map<Integer,Integer> map=new HashMap<>();
-        for (int i = 0; i <array.length ; i++) {
-            int potentialMatch=targetValue-array[i];
-            if (map.containsKey(potentialMatch)) return new int[] {i,map.get(potentialMatch)};
-            else map.put(array[i], i);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            int potentialMatch = targetValue - array[i];
+            if (map.containsKey(potentialMatch)) {
+                return new int[]{i, map.get(potentialMatch)};
 
+            } else {
+                map.put(array[i], i);
+            }
         }
         return new int[]{};
     }
