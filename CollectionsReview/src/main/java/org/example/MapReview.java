a35465f1ce235ff60603876ecca774cbd851be15
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-12-25 00:21:52 +0300

    MAP  review First Nonrepeating Char

--- CollectionsReview/src/main/java/org/example/MapReview.java
+++ CollectionsReview/src/main/java/org/example/MapReview.java
@@ -1,33 +1,48 @@
 package org.example;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
 
 public class MapReview {
     public static void main(String[] args) {
         // create hash map
+        Map<Integer, String> studentsMap = new HashMap<>();
 
+        // add element
 
+        studentsMap.put(1, "Jack");
+        studentsMap.put(2, "John");
+        studentsMap.put(3, "Tom");
+        studentsMap.put(4, "Maria");
 
-
+        String str = "JJJavvaDDevelloopperr";
+        System.out.println(firstNonRepeatingChar(str));
 
     }
-        public static Character firstRepeatingChar(String str) {
-            // Time Complexity O(n)
-            //create a hashset
-            Set<Character> setChar = new LinkedHashSet<>();
-            // Space Complexity O(n)
 
+    public static Character firstNonRepeatingChar(String str) {
+        // Time Complexity O(n)
+        //create a hashMap
+        Map<Character, Integer> countMap = new TreeMap<>();
+        // Space Complexity O(n)
 
-            // iterate over the char array and add chars into hashSet
-            for (Character each : str.toCharArray()) {
-                // if add ops is false return char
-                if (!(setChar.add(each))) {
-                    return each;
-                }
-
+        // frewuency of chars
+        int count ;
+        for (Character each : str.toCharArray()) {
+            if (countMap.containsKey(each)) {
+                count = countMap.get(each);
+                countMap.put(each, ++count);
+            } else {
+                countMap.put(each, 1);
             }
-            return null;
+        }
+//start from beginngi and chec there is a char with frequency 1
 
+        for (Character each : str.toCharArray()) {
+            if (countMap.get(each) == 1) {
+                return each;
+            }
+
+        }
+        return null;
     }
 }
