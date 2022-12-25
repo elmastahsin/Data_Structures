e8cc0520f5e7460aa94341578eeda5a0b9157fc2
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-12-25 13:20:08 +0300

    Dsa 3 LinkedList

---
+++ dsa_03_LinkedList/src/main/java/org/example/DemoClass.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class DemoClass {
+    int id;
+
+}
---
+++ dsa_03_LinkedList/src/main/java/org/example/MainApp.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class MainApp {
+    public static void main(String[] args) {
+        DemoClass cl = new DemoClass();
+
+        System.out.println(cl);
+    }
+}
