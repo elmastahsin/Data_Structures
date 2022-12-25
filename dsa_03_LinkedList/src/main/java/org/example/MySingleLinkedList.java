65f7c00bd1220d615728452f088bef2b07474609
tyson717 <111126837+tyson717@users.noreply.github.com>
2022-12-25 15:08:33 +0300

    Dsa 3 LinkedList

--- dsa_03_LinkedList/src/main/java/org/example/MySingleLinkedList.java
+++ dsa_03_LinkedList/src/main/java/org/example/MySingleLinkedList.java
@@ -5,7 +5,33 @@ public class MySingleLinkedList {
     Node head;
     Node tail;
     int size;
-    boolean isEmpty(){
-        return head==null;
+
+    boolean isEmpty() {
+        return head == null;
+    }
+
+    void add(int data) {
+        // create a new node object from data
+        Node node = new Node(data);
+        if (isEmpty()) {// if the list is empty
+            head = tail = node;
+            size++;
+        } else { // if there are elements in the list
+            tail.next = node;
+            tail = node;
+            size++;
+        }
+    }
+
+    void printNodes() {
+        Node current = head;
+        while (current != null) {
+            if (current.next == null) {
+                System.out.println(current.id + " => null");
+            }
+            System.out.println(current.id + " =>");
+            current = current.next;
+
+        }
     }
 }
