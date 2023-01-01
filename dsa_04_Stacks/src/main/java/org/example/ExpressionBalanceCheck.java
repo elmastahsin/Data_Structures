843c678584084033b13003dab8ce0260d66e87dd
tyson717 <111126837+tyson717@users.noreply.github.com>
2023-01-01 18:18:33 +0300

    Dsa 4 Stack MyStack ExpressionBalanceCheck

--- dsa_04_Stacks/src/main/java/org/example/ExpressionBalanceCheck.java
+++ dsa_04_Stacks/src/main/java/org/example/ExpressionBalanceCheck.java
@@ -4,34 +4,38 @@ public class ExpressionBalanceCheck {
     public static void main(String[] args) {
         System.out.println(balanceCheck("(A*C) + ({V-M})"));
     }
-    public static boolean balanceCheck (String expr){
+
+    public static boolean balanceCheck(String expr) {
         // create a stack
-        MyStack<Character> myStack=new MyStack<>();
+        MyStack<Character> myStack = new MyStack<>();
 
         // iterate over expr
-        for (int i = 0; i <expr.length() ; i++) {
-            Character ch=expr.charAt(i);
+        for (int i = 0; i < expr.length(); i++) {
+            Character ch = expr.charAt(i);
             // filter non player chars
-            if (ch!='('&& ch!='['&& ch!='{'&& ch!=')'&& ch!=']'&& ch!='}' ) continue ;
+            if (ch != '(' && ch != '[' && ch != '{' && ch != ')' && ch != ']' && ch != '}') continue;
             // if it is a symbol tobe processed
             // if opening symbol push it to the stack
-            if(ch=='('|| ch=='['|| ch=='{') {
+            if (ch == '(' || ch == '[' || ch == '{') {
                 myStack.push(ch);
                 continue;
             }
             // at this point this is a closing symbol
             // stack shouldnt be empty
-            if (myStack.isEmpty()) return false;
-            switch(ch) {
-                case ')':
-                    if(myStack.pop()!='(') return false;
-                    break;
-                case ']':
-                    if(myStack.pop()!='[') return false;
-                    break;
-                case '}':
-                    if(myStack.pop()!='{') return false;
-                    break;
+            if (myStack.isEmpty()) {
+                return false;
+            } else {
+                switch (ch) {
+                    case ')':
+                        if (myStack.pop() != '(') return false;
+                        break;
+                    case ']':
+                        if (myStack.pop() != '[') return false;
+                        break;
+                    case '}':
+                        if (myStack.pop() != '{') return false;
+                        break;
+                }
             }
         } // end of for
 
