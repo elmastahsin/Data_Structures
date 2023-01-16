83841d19b25c329cb6467027d30a49c5a53d754b
tyson717 <111126837+tyson717@users.noreply.github.com>
2023-01-16 12:52:48 +0300

    Dsa 6 Queues-Hashing

---
+++ dsa_06_Queues_Hashing/src/main/java/org/example/queueQuestion/QNode.java
@@ -0,0 +1,11 @@
+package org.example.queueQuestion;
+
+public class QNode<T> {
+    T value;
+    QNode<T> next;
+
+    public QNode(T value) {
+        this.value = value;
+    }
+
+}
--- dsa_6_Queues_Hashing/src/main/java/org/example/queueQuestion/ReverseKthElementInQueue.java
+++
@@ -1,7 +0,0 @@
-package org.example.queueQuestion;
-
-public class ReverseKthElementInQueue {
-    public static void main(String[] args) {
-
-    }
-}
