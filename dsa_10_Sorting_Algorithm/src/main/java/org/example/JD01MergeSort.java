9ab60cdd38454ea7657d3a38e37b76652f7e4a29
tyson717 <111126837+tyson717@users.noreply.github.com>
2023-01-21 18:05:48 +0300

    Dsa 10 SortingAlgorithm MergeSort

---
+++ dsa_10_Sorting_Algorithm/src/main/java/org/example/JD01MergeSort.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class JD01MergeSort {
+
+
+    public static void mergeSort(int[] array) {
+        if (array.length < 2) return;
+        int mid = array.length / 2;
+        int[] left = new int[mid];
+        for (int i = 0; i < mid; i++) {
+            left[i] = array[i];
+        }
+        int[] right = new int[array.length - mid];
+        for (int i = mid; i < array.length; i++) {
+            right[i] = array[i];
+        }
+        mergeSort(left);
+        mergeSort(right);
+        merge(left, right, array);
+    }
+
+    public static void merge(int[] left, int[] right, int[] result) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) result[k++] = left[i++];
+            else result[k++] = right[j++];
+        }
+        while (i < left.length) {
+            result[k++] = left[i++];
+        }
+        while (j < right.length) {
+            result[k++] = right[j++];
+        }
+    }
+
+
+}
