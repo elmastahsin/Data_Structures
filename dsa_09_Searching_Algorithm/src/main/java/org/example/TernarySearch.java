c5ab430504ccef2ee2d8539385c13cf34246a57c
tyson717 <111126837+tyson717@users.noreply.github.com>
2023-01-21 14:42:36 +0300

    Dsa 9 SearchAlgorithm TernarySearch

--- dsa_09_Searching_Algorithm/src/main/java/org/example/TernarySearch.java
+++ dsa_09_Searching_Algorithm/src/main/java/org/example/TernarySearch.java
@@ -4,23 +4,23 @@ public class TernarySearch {
     public static void main(String[] args) {
         int[] nums = {3, 2, 1, 5, 6, 4};
         System.out.println(ternarySearch(nums, 6));
-        System.out.println(ternarySearch(nums, 6));
+        System.out.println(ternarySearchRecursive(nums, 6, 0, nums.length-1));
     }
 
     public static int ternarySearch(int[] array, int data) {
         int left = 0;
-        int right = array.length;
-        int partitionSize = (left + right) / 3;
-        int mid1 = left + partitionSize;
-        int mid2 = right - partitionSize;
-        while (left <= right) {
-            if (array[mid1] == data) return mid1;
-            if (array[mid1] == data) return mid2;
-            if (data < array[mid1]) right = mid1 - 1;
-            else if (data > mid2) left = mid2 + 1;
+        int right = array.length-1;
+        while (right >= left) {
+            int partition = (right - left) / 3;
+            int middle1 = left + partition;
+            int middle2 = right - partition;
+            if (array[middle1] == data) return middle1;
+            if (array[middle2] == data) return middle2;
+            if (data < array[middle1]) right = middle1 - 1;
+            else if (data > middle2) left = middle2 + 1;
             else {
-                left = mid1 + 1;
-                right = mid2 - 1;
+                left = middle1 + 1;
+                right = middle2 - 1;
             }
         }
         return -1;
@@ -28,15 +28,15 @@ public class TernarySearch {
 
     public static int ternarySearchRecursive(int[] array, int data, int left, int right) {
         if (left > right) return -1;
-        int partitionSize = (left + right) / 3;
-        int mid1 = left + partitionSize;
-        int mid2 = right - partitionSize;
+        int partitionSize = (right -left) / 3;
+        int middle1 = left + partitionSize;
+        int middle2 = right - partitionSize;
 
-        if (array[mid1] == data) return mid1;
-        if (array[mid1] == data) return mid2;
-        if (data < array[mid1]) return ternarySearchRecursive(array, data, left, mid1 - 1);
-        else if (data > mid2) return ternarySearchRecursive(array, data, mid2 + 1, right);
-        else return ternarySearchRecursive(array, data, mid1 + 1, mid2 - 1);
+        if (array[middle1] == data) return middle1;
+        if (array[middle2] == data) return middle2;
+        if (data < array[middle1]) return ternarySearchRecursive(array, data, left, middle1 - 1);
+        else if (data > middle2) return ternarySearchRecursive(array, data, middle2 + 1, right);
+        else return ternarySearchRecursive(array, data, middle1 + 1, middle2 - 1);
 
     }
 }
