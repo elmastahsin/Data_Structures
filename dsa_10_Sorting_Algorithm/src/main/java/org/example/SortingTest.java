f5829bdc68048d2472c7dcff980a2699986a4e69
tyson717 <111126837+tyson717@users.noreply.github.com>
2023-01-21 17:47:53 +0300

    Dsa 10 SortingAlgorithm

--- dsa_10_Sorting_Algorithm/src/main/java/org/example/SortingTest.java
+++ dsa_10_Sorting_Algorithm/src/main/java/org/example/SortingTest.java
@@ -4,13 +4,13 @@ import java.util.Arrays;
 
 public class SortingTest {
     public static void main(String[] args) {
-        int[] numbers= {7,2,4,6,1,9,-1};
-        System.out.println(Arrays.toString(BubbleSort.bubbleSort(numbers)));
-        System.out.println(Arrays.toString(SelectionSort.selectionSort(numbers)));
-        System.out.println(Arrays.toString(InsertionSort.insertionSort(numbers)));
+        int[] numbers = {7, 2, 4, 6, 1, 9, -1};
+        System.out.println("BubbleSort:\t\t" + Arrays.toString(BubbleSort.bubbleSort(numbers)));
+        System.out.println("SelectionSort:\t" + Arrays.toString(SelectionSort.selectionSort(numbers)));
+        System.out.println("InsertionSort:\t" + Arrays.toString(InsertionSort.insertionSort(numbers)));
         // JD1MergeSort.mergeSort(numbers);
         //JD1QuickSort.qSort(numbers, 0, numbers.length-1);
-       // System.out.println(Arrays.toString(numbers));
+        // System.out.println(Arrays.toString(numbers));
 
     }
 }
